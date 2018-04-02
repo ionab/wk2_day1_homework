2727f3f61d3a17f4cf5bb48c719b8d38607fdcb4
Kelsie Braidwood <kelsieiona@gmail.com>
2018-04-02 16:52:20 +0100

    testing functions for codeclan students all passing - part A of lab complete

--- codeclan_student/codeclan_student.rb
+++ codeclan_student/codeclan_student.rb
@@ -9,7 +9,6 @@ class Student
     @student_name = student_name
     @student_cohort = student_cohort
   end
-
  def student_name
    return @student_name
  end
@@ -19,7 +18,13 @@ class Student
  def set_student_name(name)
     @student_name = name
  end
+ def set_student_cohort(cohort)
+   @student_cohort = cohort
+ end
  def student_can_talk
    return "I can talk!"
+ end
+ def say_favourite_language(language)
+   return "I love #{language}"
  end
 end
--- codeclan_student/specs/spec_codeclan_students.rb
+++ codeclan_student/specs/spec_codeclan_students.rb
@@ -1,19 +1,6 @@
 require ('minitest/autorun')
 require_relative('../codeclan_student.rb')
 
-### Part A
-
-# For this part we want you to make a class that represents a CodeClan student.
-#
-# * Create a class called Student that takes in a name
-#(String) and a cohort (integer) when an new instance is created.
-# * Create a couple of Getter methods, one that returns the name
-#property and one that returns the cohort property of the student.
-# * Add in Setter methods to update the students name and what cohort they are in.
-# * Create a method that gets the student to talk (eg. Returns "I can talk!).
-# * Create a method that takes in a students favourite programming
-#language and returns it as part of a string (eg. `student1.say_favourite_language("Ruby")` -> "I love Ruby").
-
 class TestStudent < MiniTest::Test
   def setup()  # you can use this setup function, which will run at the start of every test
       @codeclan_student = Student.new("Kelsie", 5)
@@ -28,7 +15,14 @@ class TestStudent < MiniTest::Test
     @codeclan_student.set_student_name("Iona")
     assert_equal("Iona", @codeclan_student.student_name)
   end
+  def test_student_cohort
+    @codeclan_student.set_student_cohort("6")
+    assert_equal("6", @codeclan_student.student_cohort)
+  end
   def test_if_student_can_talk
     assert_equal("I can talk!", @codeclan_student.student_can_talk)
+  end
+  def test_say_favourite_language
+    assert_equal("I love Python", @codeclan_student.say_favourite_language("Python"))
   end
 end
